a813e202b7718ba66aeafd3cb703a2ed957ae729
Varun Salat <varun.salat@thegatewaycorp.co.in>
2024-06-19 19:54:01 +0530

    abiyt us

--- about-us.php
+++ about-us.php
@@ -3,13 +3,12 @@ require("./function/function.php");
 
 view("header","general");
 view("hero_section","about-us");
-view("packge_section","tour");
+view("about_us_section","about-us");
 view("trending_section","general");
 view("go_to_track_section","tour");
 view("testimonial","general");
 view("why_choose_us","general");
 
 view("footer","general");
-
 
 ?>
---
+++ views/about-us/about_us_section.view.php
@@ -0,0 +1,10 @@
+<section class="section flex" id="about_us_section">
+    <figure class="about_us_left_container">
+        <img src="./img/logo.webp" alt="about-us-img">
+    </figure>
+    <div class="about_us_right_container">
+        <div>
+            <h5>About Us</h5>
+        </div>
+    </div>
+</section>
